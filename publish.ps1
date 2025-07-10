ba387db72de8a7aa15b700bc096a639760548ae6
Spencer Farley <spencerf@wcrimail.com>
2025-07-10 15:01:38 -0500

    Update package Id

--- publish.ps1
+++ publish.ps1
@@ -1,17 +1,18 @@
 Param(
   [switch]$SkipDuplicationCheck
 )
-pushDir
+Push-Location
 cd $PSScriptRoot
 dotnet pack --include-source
 if($lastexitcode -eq 0){
     $f=dir *.nupkg -rec | sort LastWriteTime -desc | select -first 1
-    ## NOTE: The WCRI_GITHUB_PACKAGES_PASSWORD is a Github classic personal access token with package write priivleges
-    $url = "https://nuget.pkg.github.com/WesternCapital/index.json"
+    ## NOTE: The STARLYCODE_GITHUB_PACKAGES_PASSWORD is a Github classic personal access token with package write priivleges
+    $f
+    $url = "https://nuget.pkg.github.com/StarlyCode/index.json"
     if($SkipDuplicationCheck){
-        dotnet nuget push $f --source $url --api-key $env:WCRI_GITHUB_PACKAGES_PASSWORD --skip-duplicate
+        dotnet nuget push $f --source $url --api-key $env:STARLYCODE_GITHUB_PACKAGES_PASSWORD --skip-duplicate
     }else{
-        dotnet nuget push $f --source $url --api-key $env:WCRI_GITHUB_PACKAGES_PASSWORD
+        dotnet nuget push $f --source $url --api-key $env:STARLYCODE_GITHUB_PACKAGES_PASSWORD
     }
     if($LASTEXITCODE -eq 0){
         Write-Host "Successfully pushed package to Github Packages"
@@ -21,4 +22,4 @@ if($lastexitcode -eq 0){
 }else{
     Write-Error "Failed to pack package: $LASTEXITCODE"
 }
-popdir
+Pop-Location
